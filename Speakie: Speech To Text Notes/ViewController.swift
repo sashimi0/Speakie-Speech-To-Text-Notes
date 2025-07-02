44a54f99623505c764b43364909348af19565379
Sasha <6260727+sashimi0@users.noreply.github.com>
2025-07-02 08:58:35 -0700

    Initial Commit

---
+++ Speakie: Speech To Text Notes/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Speakie: Speech To Text Notes
+//
+//  Created by sasha on 7/2/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Speakie: Speech To Text NotesTests/Speakie__Speech_To_Text_NotesTests.swift
@@ -0,0 +1,17 @@
+//
+//  Speakie__Speech_To_Text_NotesTests.swift
+//  Speakie: Speech To Text NotesTests
+//
+//  Created by sasha on 7/2/25.
+//
+
+import Testing
+@testable import Speakie__Speech_To_Text_Notes
+
+struct Speakie__Speech_To_Text_NotesTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
